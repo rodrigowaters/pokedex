8f72f5f4061ff8f27cad1d3d01db58c71e32a88e
Rodrigo Silva <rodrigo.silva@solucoesglobais.com.br>
2014-05-27 17:46:26 -0300

    Novo layout

--- js/app.js
+++ js/app.js
@@ -1,34 +1,39 @@
 var pokedeck = {};
-function view(id) {
-    $.mobile.loading('show');
-    var page_detals = "#detals";
-    var pokemon = pokedeck[id];
-    $(page_detals).find('div[data-role="header"] h1').text(pokemon.name);
-    var detals = $(page_detals).find('div[role="main"]');
-    detals.find('#image img').prop('src', 'images/pokemons/' + pokemon.link);
-    for (var i in pokemon) {
-        detals.find('#' + i).val(pokemon[i]);
-    }
-    $.mobile.changePage(page_detals, {transition: "flow", changeHash: false});
-    $.mobile.loading("hide");
+function loading() {
 }
-$.mobile.document.on("pageinit", "#dashboard", function() {
-    $.mobile.loading('show');
+function back() {
+    $('#view').removeClass('current').addClass('left');
+    $('#index').removeClass('left').addClass('current');
+}
+function view(id) {
+    var pokemon = pokedeck[id];
+    var form = $('#view');
+    form.find("header h1").text(pokemon.name);
+    form.find('article header img').prop('src', 'images/pokemons/' + pokemon.link);
+    for (var i in pokemon) {
+        form.find('article #' + i).val(pokemon[i]);
+    }
+    $('#view').addClass('current');
+    $('#index').addClass('left');
+}
+$(document).ready(function() {
     $.getJSON('pokemon.json', {}, function(json) {
         pokedeck = json[0];
         for (var i in pokedeck) {
-            var number = pokedeck[i].number;
-            var name = pokedeck[i].name;
-            var image = pokedeck[i].link;
-            $('#conteiner ul').append('<li onclick="view(' + i + ')">\n\
-                                            <a href="#">\n\
-                                                <img src="images/pokemons/' + image + '">\n\
-                                                <h2>' + name + '</h2>\n\
-                                                <p>#' + number + '</p>\n\
-                                             </a>\n\
-                                        </li>');
+            var html = '<li onclick="view(' + i + ')"><aside class="pack-end"><p>#' + (pokedeck[i].number) + '</p></aside><a href="#"><p>' + (pokedeck[i].name) + '</p></a></li>';
+            $('#index article ul').append(html);
         }
-        $('#conteiner ul').listview("refresh");
-        $.mobile.loading("hide");
+        $('#index form input').keyup(function() {
+            var text = $.trim($(this).val());
+            if (text) {
+                $('#index article ul li').hide();
+                $("#index article ul li a p:first-child:contains('" + (text.toUpperCase()) + "')").closest('li').fadeIn();
+            } else {
+                $('#index article ul li').show();
+            }
+        });
+        $('#index form button').click(function() {
+            $('#index article ul li').show();
+        });
     });
 });
